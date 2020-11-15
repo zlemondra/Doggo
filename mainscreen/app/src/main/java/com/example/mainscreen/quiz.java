d52f0f86f64b9ce7d413cfb84611f30028148bc1
zlemondra <44854321+zlemondra@users.noreply.github.com>
2020-11-14 19:33:13 -0800

    passes list of dogs from quiz page to other activity

--- mainscreen/app/src/main/java/com/example/mainscreen/quiz.java
+++ mainscreen/app/src/main/java/com/example/mainscreen/quiz.java
@@ -1,17 +1,26 @@
 package com.example.mainscreen;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.DoubleSummaryStatistics;
+
 public class quiz extends Activity {
     Button savebttn;
     Intent intent;
     EditText breed, gender,color, size, age;
-
+    Cursor cursor;
+    SQLiteDatabase ggdDatabase;
+    Bundle extras = new Bundle();
+    public Cursor returnMatchesIDs(){
+        return this.cursor;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //save button initization
@@ -28,7 +37,19 @@ public class quiz extends Activity {
         savebttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(quiz.this, MainActivity.class); // will change when laurn updates his part
+                cursor = ggdDatabase.rawQuery("SELECT ID FROM dogProfile WHERE gender = " + gender.getText().toString() + " AND breed = " + breed.getText().toString() + " AND age = " + age.getText().toString() + ";", null);
+                String[] dogIDs = new String[cursor.getCount()];
+                for(int i = 0; i< cursor.getCount(); i++){
+                    dogIDs[i] = cursor.getString(0);
+                    cursor.moveToNext();
+                }
+
+                ggdDatabase.close();
+
+                extras.putStringArray("Dog IDs", dogIDs);
+                // LAUREN, CHANGE THE CLASS TO YOUR DOG CLASS
+                intent = new Intent(quiz.this, MainActivity.class); // will change when lauren updates his part
+                intent.putExtras(extras);
                 startActivity(intent);
             }//End of method onClick
         });
