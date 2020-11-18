d55e329cd3d5b303d7bd9d1c855a52390418cf4c
Ashton Mulkey <ascee21@yahoo.com>
2020-11-18 13:45:31 -0800

    changes_2020-11-18
    activity_human_user_welcome
    o Added hard-coded 'View Maps' to strings.xml
    o On btn_view_map, I changed the vertical constraint so the buttons would not overlap
    
    MainActivity
    o On line 128, I redirected the intent to open ShelterSignUpActivity
    
    Android Manifest
    o On line 34, I made ,activity> a self closing tag

--- mainscreen/app/src/main/java/com/example/mainscreen/HumanUserWelcomeActivity.java
+++ mainscreen/app/src/main/java/com/example/mainscreen/HumanUserWelcomeActivity.java
@@ -4,13 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HumanUserWelcomeActivity extends AppCompatActivity {
     //Variable Declarations
     Button btnMatchMe;
+    Button btnViewMap;
     Intent intent;
 
     @Override
@@ -20,6 +20,7 @@ public class HumanUserWelcomeActivity extends AppCompatActivity {
 
         //Variable Initializations
         btnMatchMe = (Button) findViewById(R.id.btn_match_me);
+        btnViewMap = (Button) findViewById(R.id.btn_view_map);
 
         btnMatchMe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +28,17 @@ public class HumanUserWelcomeActivity extends AppCompatActivity {
                 intent = new Intent(HumanUserWelcomeActivity.this, quiz.class);
                 startActivity(intent);
             }//End of method onClick
-        });//End of btnSignUp.setOnClickListener
+        });//End of btnMatchMe.setOnClickListener
+
+
+
+        btnViewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(HumanUserWelcomeActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }//End of method onClick
+        });//End of btnViewMap.setOnClickListener
+
     }//End of method onCreate
 }//End of class HumanUserWelcomeActivity
